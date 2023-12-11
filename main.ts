e1db013d654f92ec3b9816eab466960c86b79201
Volodymyr the destroyer of dwarf <152634784+VladtheGeneral@users.noreply.github.com>
2023-12-11 17:45:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+let Number2 = 0
+input.onGesture(Gesture.Shake, function () {
+    Number2 = randint(1, 3)
+    if (Number2 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (Number2 == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
